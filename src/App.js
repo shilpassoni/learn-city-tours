c2fd2da3d6d9d018237bf0e14b2e2d0e1302c005
Shilpa Soni <shilpassoni@gmail.com>
2020-04-02 00:02:45 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.scss';
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import Navbar from "./components/Navbar/Navbar";
+import TourList from './components/TourList';
+
+class App extends Component {
+  render(){
+    return (
+      <React.Fragment>
+        <Navbar/>
+        <TourList />
+      </React.Fragment> 
+    )
+  }
+}
+
+export default App;
